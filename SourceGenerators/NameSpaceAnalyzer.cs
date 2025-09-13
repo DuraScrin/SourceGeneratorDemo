a2d9df1c247b19b2eade225977f6f5e34b454514
DuraScrin <cccp_dura@hotmail.com>
2025-09-13 13:51:18 +0200

    Test console app

---
+++ SourceGenerators/NameSpaceAnalyzer.cs
@@ -0,0 +1,36 @@
+﻿namespace SourceGenerators;
+
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+[Generator]
+public class NameSpaceAnalyzer : IIncrementalGenerator
+{
+    private const string ClassName = "MyClass.g.cs";
+    private const string ClassBody = """
+                                     namespace SourceGenerators;
+                                     
+                                     // using {{namespace}};
+                                     
+                                     public class MyClass
+                                     {
+                                         public MyClass()
+                                         {
+                                             throw new Exception("Wrong using statment detected.");
+                                         }
+                                     }
+                                     """;
+
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        context.RegisterSourceOutput(
+            context.CompilationProvider,
+            (ctx, compilation) =>
+            {
+                ctx.AddSource(ClassName, SourceText.From(ClassBody, Encoding.UTF8));
+
+                //ctx.ReportDiagnostic(Diagnostic.Create(ErrorDescriptor.WrongUsingError, Location.None));
+            });
+    }
+}
---
+++ TestConsoleApp/Generated/net9.0/SourceGenerators/SourceGenerators.NameSpaceAnalyzer/MyClass.g.cs
@@ -0,0 +1,11 @@
+﻿namespace SourceGenerators;
+
+// using {{namespace}};
+
+public class MyClass
+{
+    public MyClass()
+    {
+        throw new Exception("Wrong using statment detected.");
+    }
+}
