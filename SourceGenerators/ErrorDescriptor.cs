397d4e256d57a36d0eb80be74c63d98ba2b0835e
Duraware <duraware@durawares-mbp.home>
2025-09-13 13:50:09 +0200

    Init with debugging

--- SourceGenerators/ErrorDescriptor.cs
+++ SourceGenerators/ErrorDescriptor.cs
@@ -1,6 +1,16 @@
 namespace SourceGenerators;
 
-public class ErrorDescriptor
+using Microsoft.CodeAnalysis;
+
+internal static class ErrorDescriptor
 {
-    
+    internal static DiagnosticDescriptor WrongUsingError => new
+    (
+        id: "GG001",
+        title: "Wrong using statement detected",
+        messageFormat: "Wrong using statement detected.",
+        category: "SourceGenerators",
+        DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
 }
